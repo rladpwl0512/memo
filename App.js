ef0e99e4c159d0319052909cd52375dbe9319a92
KIM YEJI <rladpwl0512@gmail.com>
2023-06-16 13:15:47 +0900

    chore: 초기세팅

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isReadyFont, setIsReadyFont] = useState(false);
+
+  const getFonts = async () => {
+    await Font.loadAsync({
+      normal: require("./assets/fonts/gangwon.otf"),
+      point: require("./assets/fonts/UhBeeZZIBA-Regular.ttf"),
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hey!!! up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
