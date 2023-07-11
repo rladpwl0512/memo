4d8d38ee865f0ec292ba135a6043e0c1f91c2391
KIM YEJI <rladpwl0512@gmail.com>
2023-07-11 18:35:38 +0900

    feat: 실험 종료 후, 어플 종료

--- screens/FinishScreen.jsx
+++ screens/FinishScreen.jsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
-import { StyleSheet } from "react-native";
+import { BackHandler, StyleSheet } from "react-native";
 import Description from "../components/Description";
 import LottieView from "lottie-react-native";
 import disableBack from "../utils/disableBack";
 
-function FinishScreen({ navigation }) {
+function FinishScreen() {
   useEffect(() => {
     disableBack();
   }, []);
 
   const handleButtonClick = () => {
-    navigation.navigate("Login");
+    BackHandler.exitApp();
   };
 
   return <Description image={<LottieView style={styles.animation} source={require("../assets/animation/finish.json")} autoPlay loop />} titleText="연구에 참여해주셔서 감사합니다" contentText={`궁금하신 점이 있다면\ninyoung0727@swu.ac.kr로 연락주세요.\n아래의 완료 버튼을 눌러 종료하여 주세요.`} handleButtonClick={handleButtonClick} buttonText="완료" />;
