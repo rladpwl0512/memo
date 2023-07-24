4af9728575f1a15ea8c959e6a881090c3affe67a
KIM YEJI <rladpwl0512@gmail.com>
2023-07-25 02:35:11 +0900

    style: scroll 추가

--- components/Description.jsx
+++ components/Description.jsx
@@ -1,19 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import global from "../styles/globalStyle";
 import GreenButton from "../components/GreenButton";
 
 function Description({ image, titleText, contentText, handleButtonClick, buttonText }) {
   return (
-    <View style={[global.container, styles.container]}>
-      {image}
-      <Text style={[global.title, styles.title]}>{titleText}</Text>
-      <Text style={[global.content, styles.content]}>{contentText}</Text>
-      <GreenButton text={buttonText} width="40%" onClick={handleButtonClick} />
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={[global.container, styles.container]}>
+        {image}
+        <Text style={[global.title, styles.title]}>{titleText}</Text>
+        <Text style={[global.content, styles.content]}>{contentText}</Text>
+        <GreenButton text={buttonText} width="40%" onClick={handleButtonClick} />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
+
   container: {
     gap: 24,
     justifyContent: "center",
