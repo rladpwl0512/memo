e82686583008ab861325c4f5a704d3f06fac1345
KIM YEJI <rladpwl0512@gmail.com>
2023-08-09 18:06:59 +0900

    style: 실험 설명 수정

--- screens/SecondExperimentDescriptionScreen.jsx
+++ screens/SecondExperimentDescriptionScreen.jsx
@@ -12,7 +12,7 @@ function SecondExperimentDescriptionScreen({ navigation }) {
     navigation.navigate("SecondExp");
   };
 
-  return <Description image={<Image style={styles.animation} source={require("../assets/animation/second.gif")} />} titleText="두번째 실험" contentText={`제시된 목소리의 감정을 맞추는 실험입니다.\n\n목소리를 잘 듣고, 목소리의 감정을\n가장 잘 설명하는 단어를 고르세요.\n\n이어폰이나 헤드셋을 착용하고,\n소리에 집중할 수 있는 공간에서\n실험을 실시 해주세요.\n\n아래의 버튼을 누르면\n연습 시행이 2회 실시 됩니다.`} handleButtonClick={handleButtonClick} buttonText="시작" />;
+  return <Description image={<Image style={styles.animation} source={require("../assets/animation/second.gif")} />} titleText="두번째 실험" contentText={`제시된 목소리의 감정을 맞추는 실험입니다.\n\n목소리를 잘 듣고, 목소리의 감정을\n가장 잘 설명하는 단어를 고르세요.\n\n이어폰이나 헤드셋을 착용하고,\n소리에 집중할 수 있는 공간에서\n실험을 실시 해주세요.\n\n두번째 실험은 16문항이며,\n단어 선택지는 10초 간 지속됩니다.\n\n아래의 버튼을 누르면\n연습 시행이 2회 실시 됩니다.`} handleButtonClick={handleButtonClick} buttonText="시작" />;
 }
 
 const styles = StyleSheet.create({
